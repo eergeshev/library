aba75d29063f9b148217d0f399c8c4cebec8737b
Ernis <eergeshev@gmail.com>
2020-09-02 19:33:10 +0600

    Kyrgyzstan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Kyrgyzstan
                 </div>
 
                 <div class="links">
